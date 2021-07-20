adc851144b777aa3e9afeea2a7d1b6524ccc31b8
EalenXie <ealenxie@qq.com>
2021-07-20 09:35:24 +0800

    移除RestTemplateProxy

--- src/main/java/com/github/proxy/RestTemplateCollector.java
+++
@@ -1,9 +0,0 @@
-package com.github.proxy;
-
-import com.github.Collector;
-
-/**
- * @author EalenXie create on 2021/1/4 16:56
- */
-public interface RestTemplateCollector extends Collector<ReqInfo> {
-}
